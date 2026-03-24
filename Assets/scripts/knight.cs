7d462c0adf202b4c854f409b374a1d00a94a78be
zhang278 <zhang278@OA-S141-23>
2026-03-24 10:51:42 -0400

    clicking

--- Assets/scripts/knight.cs
+++ Assets/scripts/knight.cs
@@ -1,8 +1,15 @@
+using System.Collections;
+using UnityEditor.Tilemaps;
 using UnityEngine;
 
 public class knight : MonoBehaviour
 {
     public AudioSource walk;
+    public Animator hero;
+    public float speed;
+    public SpriteRenderer sr;
+
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -17,6 +24,21 @@ public class knight : MonoBehaviour
 
     public void footstep()
     {
-  
+        walk.Play();
+    }
+    IEnumerable move()
+    {
+
+
+
+        yield return null;
+    }
+
+    IEnumerable attack()
+    {
+
+
+        hero.SetTrigger("attack");
+        yield return null;
     }
 }
