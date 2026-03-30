78265764494266ff199827b1eb79bb73faaf71d4
derek <zhang278@sheridancollege.ca>
2026-03-29 22:16:21 -0400

    Their guns now rotate correctly

--- Assets/Scenes/systems/systemScripts/Enemy guns.cs
+++ Assets/Scenes/systems/systemScripts/Enemy guns.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 
 public class Enemyguns : MonoBehaviour
 {
+    public player P;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -11,6 +12,7 @@ public class Enemyguns : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Vector2 direction = (Vector2)P.transform.position - (Vector2)transform.position;
+        transform.right = direction;
     }
 }
