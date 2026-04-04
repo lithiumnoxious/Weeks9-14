e71aad5114b31d48a7e1440abcdec4500812c9fe
derek <zhang278@sheridancollege.ca>
2026-04-03 21:30:50 -0400

    shot guns are now active and working

--- Assets/Scenes/systems/systemScripts/shotgun.cs
+++ Assets/Scenes/systems/systemScripts/shotgun.cs
@@ -7,6 +7,9 @@ public class shotgun : MonoBehaviour
     public float timer;
     public float timecap;
 
+    public float MaxBuck;
+
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -28,10 +31,21 @@ public class shotgun : MonoBehaviour
 
     IEnumerator shoot()
     {
+
         timer = 0;
-        //spawns bullet
-        bs.SpawnedBullet = Instantiate(bs.Bullet, transform.position, transform.rotation * Quaternion.Euler(0, 0, -90));
-        bs.Bullets.Add(bs.SpawnedBullet);
+        for (int buckshot = 0; buckshot <= MaxBuck; buckshot++)
+        {
+            float randomZ = Random.Range(-20f, 20f);
+            //spawns bullet
+            //Quaterion identity is used to make sure the bullet is going up
+            //added random range for the shotgun spread.
+
+            bs.SpawnedBullet = Instantiate(bs.Bullet, transform.position, transform.rotation * Quaternion.Euler(0, 0, -90 + randomZ));
+            bs.Bullets.Add(bs.SpawnedBullet);
+            
+            yield return new WaitForSeconds(0.05f);
+        }
+        
         yield return null;
     }
 }
