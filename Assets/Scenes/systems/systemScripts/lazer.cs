b615ac3c8f8860905d7be7cc190e3f48baff864b
derek <zhang278@sheridancollege.ca>
2026-04-04 17:24:49 -0400

    laser working

--- Assets/Scenes/systems/systemScripts/lazer.cs
+++ Assets/Scenes/systems/systemScripts/lazer.cs
@@ -1,7 +1,18 @@
+using System.Collections;
 using UnityEngine;
 
 public class lazer : MonoBehaviour
 {
+    public Transform beamtrans;
+    public float Currentlength;
+    public float Maxlength;
+
+
+
+    public float timer;
+    public float timecap;
+
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -11,6 +22,38 @@ public class lazer : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (timer >= timecap)
+        {
+            StartCoroutine(ignite());
+
+        }
+        else
+        {
+            timer += Time.deltaTime;
+        }
     }
+    IEnumerator ignite()
+    {
+        timer = 0;
+        Currentlength = 0;
+        while (Currentlength < Maxlength)
+        {
+            Currentlength += 3*Time.deltaTime;
+            beamtrans.localScale += new Vector3(0,Currentlength,0);
+            yield return new WaitForSeconds(0.05f);
+        }
+        beamtrans.localScale = new Vector3(1,0,1);
+        
+       
+        yield return null;
+    }
+
+    IEnumerator retract()
+    {
+
+
+        yield return null;
+    }
+
+
 }
