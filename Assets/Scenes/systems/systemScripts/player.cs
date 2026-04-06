b84e5f16a9bc6731193243644a2e57b0f87af345
derek <zhang278@sheridancollege.ca>
2026-04-05 21:24:12 -0400

    text

--- Assets/Scenes/systems/systemScripts/player.cs
+++ Assets/Scenes/systems/systemScripts/player.cs
@@ -22,21 +22,20 @@ public class player : MonoBehaviour
     public bool isdead = false;
 
     public TextMeshProUGUI hp;
-    //public TextMesh death;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         sr = GetComponent<SpriteRenderer>();
-
         PHp = MaxPHp;
-        //hp.text = PHp + " Hp";
     }
 
     // Update is called once per frame
     void Update()
     {
+        //updating the text
         hp.text = PHp + " Hp";
+
         if (!ishot)
         {
             //movement
